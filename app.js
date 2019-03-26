98509c1a7abd46ecf927dece0eb07f3c416b2b21
DariaZielinska <dari.zielinska@gmail.com>
2019-03-26 13:27:42 +0100

    style: added media styles to intro-section

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 document.addEventListener('DOMContentLoaded', function(){
 
+    //making menu visible after clicking on hamburger menu icon
     const intViewportWidth = window.innerWidth;
     if(intViewportWidth < 575){
         document.querySelector(".header-nav").style.display = "none"
     }
 
-
-    //making menu visible after clicking on hamburger menu icon
     document.querySelector(".fa-bars").addEventListener("click", function () {
-
         if(document.querySelector(".header-nav").style.display === "none"){
             document.querySelector(".header-nav").style.display = "block";
         }else{
